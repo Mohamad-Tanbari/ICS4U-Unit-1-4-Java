17d123d259db7194125f777037e68b1e1513c3fc
Mohamad Tanbari <mohamad.tanbari@mths.ca>
2024-10-02 13:28:29 +0000

    formatted nicely

--- Dice.java
+++ Dice.java
@@ -7,7 +7,6 @@
  */
 
 import java.util.Scanner;
-import java.util.Random;
 
 /**
  * This is the main class.
@@ -16,7 +15,7 @@ final class Dice {
     /**
      * The maximum number that can be generated.
      */
-    static final int maxRandomNumber = 6;
+    static final int MAX_RANDOM_NUMBER = 6;
 
     /**
      * Prevent instantiation.
@@ -25,20 +24,22 @@ final class Dice {
 
     /**
      * The main method.
+     *
+     * @param args terminal inputs.
      */
     public static void main(String[] args) {
         // Create scanner object to read input from user
         final Scanner scanner = new Scanner(System.in);
 
         // Generate random number
-        final int randomNumber = (int)Math.floor(Math.random()
-            * (maxRandomNumber - 1) + 1);
+        final int randomNumber = (int) Math.floor(Math.random()
+            * (MAX_RANDOM_NUMBER - 1) + 1);
 
         // Initialize attempts counter
         int attempts = 1;
 
         // Input loop
-        while(true) {
+        while (true) {
             // Initial input prompt
             System.out.printf("Guess a number between 1 - 6: ");
             final String guessStr = scanner.nextLine();
@@ -50,10 +51,10 @@ final class Dice {
 
                 // Decide what to do with the guess
                 if (guess == randomNumber) {
-                    System.out.println("You guessed the right number!" + 
-                      "\nIt took you " + attempts + " attempts.\n");
+                    System.out.println("You guessed the right number!"
+                        + "\nIt took you " + attempts + " attempts.\n");
                     break;
-                } else if (guess > maxRandomNumber) {
+                } else if (guess > MAX_RANDOM_NUMBER) {
                     System.out.println("That is not 1 - 6.\nTry again!\n");
                 } else if (guess < randomNumber) {
                     System.out.println("Your guess is too low.\nTry again!\n");
@@ -64,7 +65,7 @@ final class Dice {
                 // Add to attempts counter
                 attempts++;
             } catch (NumberFormatException error) {
-              System.out.println("\nInvalid input. Try again.\n");
+                System.out.println("\nInvalid input. Try again.\n");
             }
         }
     }
